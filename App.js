af2ee8fe2b170cbb29a7476b9ab7bce389c6ab53
MohamadKh75 <khajavi_mohamad@yahoo.com>
2019-05-30 22:35:20 +0430

    Separate Router

--- App.js
+++ App.js
@@ -1,40 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from "react";
+import Stacking from "./src/Router";
 
-import React, {Component} from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+const App = () => {
+  return <Stacking />;
+};
 
-import {createStackNavigator} from 'react-navigation'
-import Aval from "./component/Aval";
-import Dovom from "./component/Dovom";
-import Sevom from "./component/Sevom";
-import Aks from "./component/Aks";
-
-
-export default class App extends Component {
-  render() {
-    return (
-   <Stacking/>
-    );
-  }
-}
-const Stacking=createStackNavigator({
-  aks:Aks,
-  aval:Aval,
-  dovom:Dovom,
-  sevom:Sevom
-})
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-
-});
+export default App;
